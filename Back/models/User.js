a77e32cd6c0e33e57a4195fa384df68e6c7de062
Ananya <afmtrendaid@gmail.com>
2023-09-01 14:16:25 -0700

    Main User Model

---
+++ Back/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [3, 'Username must be at least 3 characters long']
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [8, 'Password must be at least 8 characters long']
+  },
+  role: {
+      type: String,
+      required: true,
+      enum: ['user', 'admin'],
+      default: 'user'
+  },
+  knowledge: [
+    {
+        type: string
+    }
+  ],
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
