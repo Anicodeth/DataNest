2622bc77c9eb0c0afd76b4a4d45eb9f65fddf099
ffekirnew <ffekirnew0808@gmail.com>
2023-09-02 15:35:04 +0300

    Implement basic UI for signin and signup

---
+++ Front/DataNest/src/presentation/components/authentication/authentication_page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AuthenticationPage = ({ illustration, pageTitle, children }) => {
+  return (<>
+    <div className='flex flex-row h-screen'>
+      <div id={`${pageTitle}-illustration`} className='h-full'>
+        <video controls={false} autoPlay loop src={illustration} className='h-full' />
+      </div>
+      <div id={`${pageTitle}-form`} className='flex-grow h-full'>
+        <div className="flex flex-col h-full w-full px-10 justify-center">
+          { children }
+        </div>
+      </div>
+    </div>
+  </>) 
+}
+
+export default AuthenticationPage
