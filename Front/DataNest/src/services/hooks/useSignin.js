e77f6d6b48036389bd5a69828aab96fba25b597f
ffekirnew <ffekirnew0808@gmail.com>
2023-09-02 16:44:39 +0300

    Change authService login and implement useAuthService hook

--- Front/DataNest/src/services/hooks/useSignin.js
+++ Front/DataNest/src/services/hooks/useSignin.js
@@ -1,11 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { login } from "../authService"
 
-const useSignin = () => {
+// credentials is an object in the format { username: string, password: string }
+const useSignIn = () => {
   const [ isSuccess, setSuccess ] = useState(false)
   const [ error, setError ] = useState("")
   const [ isLoading, setLoading ] = useState(false)
 
-  return { isSuccess, error, isLoading }
+  const signIn = async (credentials) => {
+    setError(false)
+    setLoading(true)
+
+    await login(credentials)
+    .then((res) => {
+      setLoading(false)
+      setSuccess(true)
+      console.log(res)
+    }).catch((error) => {
+      setLoading(false)
+      setSuccess(false)
+      setError(error.message)
+    })
+  }
+
+  return { isSuccess, error, isLoading, signIn }
 }
 
-export default useSignin
+export default useSignIn
