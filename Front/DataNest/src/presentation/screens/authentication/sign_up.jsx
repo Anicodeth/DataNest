fc6275a1e942705ec4b163d2d29c90c84118ef79
ffekirnew <ffekirnew0808@gmail.com>
2023-09-02 20:48:09 +0300

    Improve signup logic

--- Front/DataNest/src/presentation/screens/authentication/sign_up.jsx
+++ Front/DataNest/src/presentation/screens/authentication/sign_up.jsx
@@ -1,32 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthenticationPage from '../../components/authentication/authentication_page';
 import astronaut_achieving from '../../../assets/astronaut_achieving.mp4';
 
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import useSignUp from '../../../services/hooks/useSignUp';
+import useSignIn from '../../../services/hooks/useSignIn';
 
 const schema = z.object({
   username: z.string().min(3, { message: "Username must be at least 3 charachters." }),
   email: z.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, { message: "Email must be a valid email address." }),
   password: z.string().min(6, { message: "Password must be at least 6 charachters." })
-})
+});
 
 const SignUp = () => {
-  const { register, handleSubmit, formState: { errors, isValid } } = useForm({ resolver: zodResolver(schema) })
-  const { isLoading, isSuccess, error, signUp } = useSignUp()
+  const [ signInTried, setSignInTried ] = useState(false);
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm({ resolver: zodResolver(schema) });
+  const { user, isLoading, isSuccess, error, signUp } = useSignUp();
+  const { isLoading: signInLoading, isSuccess: signInIsSuccess, error: signInError, signIn } = useSignIn();
 
   const handleSignup = (data) => {
-    signUp(data)
+    signUp(data);
+  }
+
+  if (isSuccess & !signInTried) {
+    setSignInTried(true);
+    signIn(user.username, user.password);
+  }
+
+  if (signInIsSuccess) {
+    // route to the user page
   }
 
   return (
     <AuthenticationPage illustration={astronaut_achieving} pageTitle={'Signup'}>
       <>
-        <h1 className="text-3xl font-semibold mb-20 text-violet-600">Create an Account with us.</h1>
-        <form className="w-full max-w-md flex flex-col gap-4" onSubmit={handleSubmit(handleSignup)}>
-          <div className="mb-4 flex flex-col gap-2">
+        <h1 className="text-3xl font-semibold mb-10 text-violet-600">Create an Account with us.</h1>
+        <form className="w-full max-w-md flex flex-col gap-4 mb-10" onSubmit={handleSubmit(handleSignup)}>
+          <div className="flex flex-col gap-2">
             <label className="block text-md font-semibold text-violet-500" htmlFor="username">
               Username
             </label>
@@ -36,10 +48,11 @@ const SignUp = () => {
               type="text"
               id="username"
               placeholder="e.g. starlord"
+              disabled={isLoading | signInLoading}
             />
             { errors.username && <p className="text-red-400">{ errors.username.message }</p> }
           </div>
-          <div className="mb-4 flex flex-col gap-2">
+          <div className="flex flex-col gap-2">
             <label className="block text-md font-semibold text-violet-500" htmlFor="email">
               Email
             </label>
@@ -49,10 +62,11 @@ const SignUp = () => {
               type="text"
               id="email"
               placeholder="e.g. johndoe@email.com"
+              disabled={isLoading | signInLoading}
             />
             { errors.email && <p className="text-red-400">{ errors.email.message }</p>}
           </div>
-          <div className="mb-8 flex flex-col gap-2">
+          <div className="flex flex-col gap-2">
             <label className="block text-md font-semibold text-violet-500" htmlFor="password">
               Password
             </label>
@@ -62,19 +76,27 @@ const SignUp = () => {
               type="password"
               id="password"
               placeholder="Enter your password"
+              disabled={isLoading | signInLoading}
             />
             { errors.password && <p className="text-red-400">{ errors.password.message }</p>}
           </div>
-        <div className="mb-8">
+        <div>
           { error && <p className="text-red-400">{ error }</p>}
+          { isSuccess && <p className='text-green-700'>Sign Up was successful, now signing you in.</p> }
+          { signInError && <p className='text-red-400'>Sign up was successful, but we couldn't log you in. You can manually log in.</p> }
         </div>
-          <div className="mb-6">
-            <button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 px-6 py-3 rounded-full w-full text-white" disabled={isLoading}> 
-              <p className="font-bold">{ isLoading ? "Signing Up..." : "Sign Up" }</p>
+          <div>
+            <button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 px-6 py-3 rounded-full w-full text-white" disabled={isLoading | signInLoading}> 
+              <p className="font-bold">{ isLoading ? "Signing Up..." : signInLoading ? "Signing you in..." : "Sign Up" }</p>
             </button>
           </div>
         </form>
-        </>
+        <div>
+          <p className="text-md text-violet-600">
+            Already have an account? <a href='#' className='font-bold hover:underline'>Sign In!</a>
+          </p>
+        </div>
+      </>
     </AuthenticationPage>
   );
 };
