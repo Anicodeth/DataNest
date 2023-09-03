d589343a47d49d8db3a695d0a42c666abc4cfa67
ffekirnew <ffekirnew0808@gmail.com>
2023-09-03 07:59:13 +0300

    Add ui components to improve ui/ux

---
+++ Front/DataNest/src/presentation/components/ui/fading_dot/fading_dot.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './fading_dot.css'
+
+const FadingDot = () => {
+  return <div class="spinner"></div>
+}
+
+export default FadingDot;
---
+++ Front/DataNest/src/presentation/components/ui/loading_ripples/loading_ripples.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './loading_ripples.css';
+
+const LoadingRipples = () => {
+  return <div class="spinner">
+    <div class="bounce1"></div>
+    <div class="bounce2"></div>
+    <div class="bounce3"></div>
+  </div>
+}
+
+export default LoadingRipples;
