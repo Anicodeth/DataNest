016e63b58eec12bd8c54e41b93e29999da98b2ac
Leul Abay <leulabay1@gmail.com>
2023-09-03 08:45:41 +0300

    add background colors

--- Front/DataNest/tailwind.config.js
+++ Front/DataNest/tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    backgroundColor:{
+      primary: ["#082661"],
+      secondary:["#10c0d5"],
+      purple:["#9415d2"]
+    },
     fontFamily:{
       IMFrench:['IM Fell French Canon', "serif"],
       LeagueSpartan:['League Spartan', "sans-serif"],
