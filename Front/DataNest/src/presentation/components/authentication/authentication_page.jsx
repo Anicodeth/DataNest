550896335401b535d087304a26084fa4c5d69696
ffekirnew <ffekirnew0808@gmail.com>
2023-09-03 08:47:14 +0300

    Improve auth ui

--- Front/DataNest/src/presentation/components/authentication/authentication_page.jsx
+++ Front/DataNest/src/presentation/components/authentication/authentication_page.jsx
@@ -7,7 +7,7 @@ const AuthenticationPage = ({ illustration, pageTitle, children }) => {
         <video controls={false} autoPlay loop src={illustration} className='h-full' />
       </div>
       <div id={`${pageTitle}-form`} className='flex-grow h-full'>
-        <div className="flex flex-col h-full w-full px-10 justify-center">
+        <div className="flex flex-col h-full w-full px-10 justify-center items-center">
           { children }
         </div>
       </div>
--- Front/DataNest/src/presentation/components/authentication/button.jsx
+++ Front/DataNest/src/presentation/components/authentication/button.jsx
@@ -4,7 +4,7 @@ const Button = ({ isLoading, type, children }) => {
   return <button
     type={type}
     disabled={isLoading}
-    class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 px-6 py-3 rounded-full w-full text-white cursor-pointer"> 
+    class="bg-[#9415d2] hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 px-6 py-3 rounded-full w-full text-white cursor-pointer"> 
     <p className="font-bold">
       { children }
     </p>
