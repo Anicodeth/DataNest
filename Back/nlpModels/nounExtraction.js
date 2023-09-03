fb93a0ed0c8cbb21295748cdb883e376a6bf51c7
Ananya <afmtrendaid@gmail.com>
2023-09-03 12:11:48 -0700

    Minor refactor

--- Back/nlpModels/nounExtraction.js
+++ Back/nlpModels/nounExtraction.js
@@ -1,19 +1,15 @@
 const nlp = require('compromise-nlp');
 
 function extractNouns(text) {
-  // Process the text using compromise-nlp
   const doc = nlp(text);
 
-  // Extract nouns from the document
   const nouns = doc.nouns().out('array');
 
-  // Convert the array of nouns to a single string
   const nounsString = nouns.join(', ');
 
   return nounsString;
 }
 
-// Example usage
 const text = "The quick brown fox jumped over the lazy dog. The lazy dog slept in the sun.";
 const extractedNouns = extractNouns(text);
 
