37113ab1c589a410591652aa13f50e8cc5fadd06
ffekirnew <ffekirnew0808@gmail.com>
2023-09-03 08:48:36 +0300

    Improve the loading ripples ui component

--- Front/DataNest/src/presentation/components/ui/loading_ripples/loading_ripples.jsx
+++ Front/DataNest/src/presentation/components/ui/loading_ripples/loading_ripples.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import './loading_ripples.css';
 
 const LoadingRipples = () => {
-  return <div class="spinner">
-    <div class="bounce1"></div>
-    <div class="bounce2"></div>
-    <div class="bounce3"></div>
+  return <div className="spinner w-full">
+    <div className="bounce1"></div>
+    <div className="bounce2"></div>
+    <div className="bounce3"></div>
   </div>
 }
 
